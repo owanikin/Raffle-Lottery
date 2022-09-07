f9ae586802b20e9515623ee2fb04112c6a2e3088
Owanikin <oderindeife@gmail.com>
2022-09-07 20:49:51 +0100

    chainlink vrf

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,14 +8,21 @@
 //  SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variable */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
-    constructor(uint256 entranceFee) {
+    /* Events */
+    event RaffleEnter(
+        address indexed player
+    );
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -24,9 +31,20 @@ contract Raffle {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external{
+        // Request the random number
+        // Once we get it, do something with it
+        // 2 transaction process
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+
+    }
+
+    /* View / Pure Functions */
 
     function getEntranceFee() public view returns(uint256) {
         return i_entranceFee;
