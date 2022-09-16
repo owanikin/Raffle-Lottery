f454ea099b3b23b67b3950dfe0ebcfa63b8a6d5d
Owanikin <oderindeife@gmail.com>
2022-09-16 17:21:26 +0100

    Raffle.sol test: Raffle is Open / Calculating

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -3,7 +3,7 @@ const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name) ? describe.skip : describe("Raffle Unit Tests", async function () {
-    let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer
+    let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval
     const chainId = network.config.chainId
 
     beforeEach(async function() {
@@ -12,6 +12,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
         raffle = await ethers.getContract("Raffle", deployer)
         vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
         raffleEntranceFee = await raffle.getEntranceFee()
+        interval = await raffle.getInterval()
     })
 
     describe("constructor", async function() {
@@ -35,6 +36,14 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
         })
         it("emits event on enter", async function() {
             await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(raffle, "RaffleEnter")
+        })
+        it("doesn't allow when raffle is calculating", async function() {
+            await raffle.enterRaffle({ value: raffleEntranceFee })
+            await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+            await network.provider.send("evm_mine", [])
+            // We pretend to be a Chainlink Keepeer 
+            await raffle.performUpkeep([])
+            await expect(raffle.enterRaffle({value: raffleEntranceFee})).to.be.revertedWith("Raffle__NotOpen")
         })
     })
 })
