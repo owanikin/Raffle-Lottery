47c1c8d2463e9f1e8f0640c071b210b04bf70325
Owanikin <oderindeife@gmail.com>
2022-09-16 19:30:58 +0100

    performUpkeep: Only run if checkUpkeep is true

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -47,13 +47,13 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
         })
     })
     describe("checkUpkeep", function() {
-        it("returns false if people haven't sent any ETH", async function() {
+        it("returns false if people haven't sent any ETH", async () => {
             await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
             await network.provider.send("evm_mine", [])
             const {upkeepNeeded} = await raffle.callStatic.checkUpkeep([])
             assert(!upkeepNeeded)
         })
-        it("returns false if raffle isn't open", async function() {
+        it("returns false if raffle isn't open", async () => {
             await raffle.enterRaffle({ value: raffleEntranceFee })
             await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
             await network.provider.send("evm_mine", [])
@@ -77,5 +77,14 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
             const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([])
             assert(upkeepNeeded)
         })
+    })
+    describe("performUpkeep", function () {
+        it("it can only run if checkUpkeep is true", async () => {
+            await raffle.enterRaffle({ value: raffleEntranceFee })
+            await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+            await network.provider.send("evm_mine", [])
+            const tx = await raffle.performUpkeep([])
+            assert(tx)
+        } )
     })
 })
