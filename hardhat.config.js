da0452c81f4d113fbc9e03dd8d0d0785f3615a48
Owanikin <oderindeife@gmail.com>
2022-09-22 22:03:13 +0100

    run mock scripts to pick a random winner

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,48 +6,51 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
 // const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY 
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      blockConfirmations: 1
+    solidity: "0.8.9",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+            blockConfirmations: 1
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [GOERLI_PRIVATE_KEY],
+        },
     },
-    goerli: {
-      chainId: 5,
-      blockConfirmations: 6,
-      url: GOERLI_RPC_URL,
-      accounts: [GOERLI_PRIVATE_KEY],
-    }
-  },
-  etherscan: {
-    apiKey: {
-      goerli: ETHERSCAN_API_KEY,
-    }
-  },
-  gasReporter: {
-    enabled: false,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
     },
-    player: {
-      default: 1,
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
     },
-  },
-  mocha: {
-    timeout: 300000,   // 300 seconds max
-  }
-};
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 300000, // 300 seconds max
+    },
+}
